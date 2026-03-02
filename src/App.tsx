29ae06ed83ec5c50239081402255444aa9c7f51a
Emmanuel Johnson <emmanuel.johnson.pro@gmail.com>
2026-03-02 22:02:39 +0530

    chore: add Prettier configuration

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ function App() {
   return (
     <>
       <div>
-        <h1 className="bg-slate-900 text-white text-4xl font-bold p-6 min-h-screen">Tailwind V4 is working</h1>
+        <h1 className="bg-slate-900 text-6xl font-bold text-white p-6 min-h-screen">
+          Tailwind V4 is working
+        </h1>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
